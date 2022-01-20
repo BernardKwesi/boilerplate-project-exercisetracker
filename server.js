3449efeffd0d26e0e7a655876eec00000cacc0c7
Bernardkwesi <52609310+BernardKwesi@users.noreply.github.com>
2022-01-20 18:57:44 +0000

    Almost Done with exercise tracker

--- server.js
+++ server.js
@@ -2,6 +2,27 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 require('dotenv').config()
+const mongoose = require("mongoose")
+const {Schema} = mongoose;
+const bodyParser = require("body-parser");
+
+mongoose.connect(process.env.MONGO_URI,  { useNewUrlParser: true, 
+useUnifiedTopology: true });
+const userSchema = new Schema({
+  "username" : String
+});
+
+const exerciseSchema = new Schema({
+  userId: String,
+  username: String,
+  description : String,
+  duration : Number,
+  date :{ type: Date, default: Date.now }
+})
+const User = new mongoose.model('User', userSchema);
+const Exercise = new mongoose.model('Exercise', exerciseSchema);
+
+app.use(bodyParser.urlencoded({extended: false}));
 
 app.use(cors())
 app.use(express.static('public'))
@@ -9,7 +30,106 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
 
+app.post("/api/users",(req,res)=>{
 
+const username  = req.body.username;
+const newUser = new User({username: username});
+
+newUser.save((err,data)=>{
+  if(err) return console.log(err);
+
+  res.json({_id:data.id, username:data.username});
+
+});
+
+        
+});
+
+app.post("/api/users/:_id/exercises",(req,res)=>{
+
+const userid = req.params._id;
+
+let username ='';
+ User.findById(userid,(err,data)=>{
+if(err) res.json("User not found");
+
+username =data.username;
+
+});
+
+
+
+const description = req.body.description;
+const duration  = req.body.duration;
+const date = req.body.date;
+const exercise = new Exercise({
+  userId: userid,
+    description: description,
+    duration : duration,
+    date : date
+});
+    exercise.save((err,data)=>{
+        if(err) return console.log(err);
+
+        res.json({
+          _id: data.userId,
+          username :username,
+          date : data.date.toDateString(),
+          duration:data.duration,
+    description:data.description
+    
+     
+        });
+    });
+
+});
+
+app.get("/api/users",(req,res)=>{
+
+  
+   User.find({},(err,data)=>{
+     if(err) return res.send('No Users Found');
+
+            res.send(data);
+  });
+
+});
+
+app.get("/api/users/:_id/logs?:from",(req,res)=>{
+
+const userid = req.params._id;
+console.log(req.query);
+const {from,to,limit} =req.query;
+
+let exercises = [];
+Exercise.find({userId:userid},(err,data)=>{
+  if(err) return console.log(err);
+  
+data.map((exercise)=>{
+  exercises.push(exercise);
+});
+
+
+ User.findById(userid,(err,data)=>{
+
+if(err) return res.send(err);
+ 
+ const username = data.username;
+
+ res.json({_id:userid,
+  username:username
+  , count:exercises.length
+  , log:exercises.map(exercise =>{
+    return ({description:exercise.description,duration:exercise.duration,date:exercise.date.toDateString()})
+  })});
+})
+
+});
+
+});
+
+
+ 
 
 
 
